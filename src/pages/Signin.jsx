18b27cb8b08f6f499c34ca3637d13638b1a2c0a2
Ak32599 <akhilesh.rachapudi98@gmail.com>
2025-11-08 21:41:47 +0530

    Update Signin.jsx

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './SignIn.css';
+import './Signin.css';
 
 const Signin = () => {
   const navigate = useNavigate();
